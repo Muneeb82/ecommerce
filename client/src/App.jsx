b6e9792e2dbec602dba6772924ef104c3a568474
Muneeb <muneebt243@gmail.com>
2025-07-14 18:44:04 +0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* Add more routes here */}
+          </Routes>
+        </main>
+        
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-2xl font-bold">Welcome to MERN eCommerce</h1>
+    </div>
+  );
+}
